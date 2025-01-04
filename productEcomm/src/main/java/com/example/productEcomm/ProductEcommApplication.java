f93b5ceeec9ae167415ad9aa4f370d3789e81b57
Md.Mehedi Hasan <ght.32rd@gmail.com>
2025-01-04 22:05:48 +0600

    Initial commit

---
+++ productEcomm/src/main/java/com/example/productEcomm/ProductEcommApplication.java
@@ -0,0 +1,13 @@
+package com.example.productEcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductEcommApplication.class, args);
+	}
+
+}
